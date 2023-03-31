57f83dfa5feb967af2efed2f59d4a0f0a54fd21d
Plínio Larrubia <plinio2xd@gmail.com>
2023-03-31 18:52:32 -0300

    JS manipulating data (pt 1)

---
+++ src/foundation/10-javascript/31/script.js
@@ -0,0 +1,2 @@
+let number = 345.23453425
+console.log(number.toFixed(2).replace('.', ','))
---
+++ src/foundation/10-javascript/33/script.js
@@ -0,0 +1,5 @@
+let phrase = 'Eu quero viver o Amor!'
+let myArray = phrase.split(' ')
+console.log(myArray)
+let phraseWithUnderscore = myArray.join('_')
+console.log(phraseWithUnderscore.toLowerCase())
