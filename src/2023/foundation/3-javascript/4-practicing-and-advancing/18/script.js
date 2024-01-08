435ebe60f76c7a3482e28fccb503ec2488be586f
LeDragoX <plinio2xd@gmail.com>
2024-01-08 18:03:04 -0300

    Review old files

--- src/2023/foundation/3-javascript/4-practicing-and-advancing/18/script.js
+++ src/2023/foundation/3-javascript/4-practicing-and-advancing/18/script.js
@@ -1,16 +1,20 @@
+// 1.
 let weight
 
+// 2. Undefined
 console.log(typeof weight)
 
+// 3.
 let name = 'Adam'
 let age = 27
 let stars = 4.5
 let isSubscribed = true
 
+// 4. Object
 let student = {}
-
 console.log(typeof student)
 
+// 4.1
 student = {
   name: name,
   age: age,
@@ -19,16 +23,21 @@ student = {
   weight: 67.9
 }
 
+// 4.2
 console.log(student)
-
 
 console.log(`${student.name} de idade ${student.age} pesa ${student.weight}kg.`)
 
+// 5.
 let students = []
 
+// 6.
 students.push(student)
+
+// 7.
 console.log(students[0])
 
+// 8.
 const john = {
   name: 'John',
   age: 17,
@@ -40,5 +49,6 @@ const john = {
 students.push(john)
 console.log(students)
 
+// 9. Undefined, pois a variável será jogada pra cima e criada sem estar com valor.
 console.log(a)
 var a = 1
